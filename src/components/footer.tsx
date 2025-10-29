6d768e4a4370c759419cab21055119394d14fd39
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2025-10-29 17:26:12 +0900

    細かいUIの更新

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="border-t mt-12 border-gray-200 dark:border-gray-700 py-12">
+    <footer className="border-t border-gray-200 dark:border-gray-700 py-12">
       <div className="container mx-auto px-4">
         <p className="text-center text-gray-700 dark:text-gray-300">
           &copy; {new Date().getFullYear()} Blueberry Quest. All rights
